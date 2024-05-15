a9587884cec44f017d037da8bbcb7f8080d6f686
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-05-15 16:42:57 +0400

    feat: added wp-cli installation

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "[info] installing wp-cli"
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/bin/wp
 
+echo "[info] downloading wordpress files"
+wp core download --allow-root
 
 php-fpm81 -F
