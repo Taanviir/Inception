53c41220a2ae421ef047f4853a5704b7c8d2b80b
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-05-17 20:01:57 +0400

    feat: wp now creates config file

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -8,12 +8,18 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/bin/wp
 
-if ! wp core is-installed; then
+if ! wp core is-installed 2>/dev/null; then
     echo "[info] wp is not installed, downloading wordpress files"
     wp core download --allow-root
 else
     echo "[info] wp is already installed"
 fi
 
+echo "[info] creating wordpress config file"
+wp config create --allow-root \
+                 --dbname=$MARIADB_DATABASE \
+                 --dbuser=$WP_DB_USER \
+                 --dbpass=$WP_DB_PASSWORD \
+                 --dbhost=$WP_DB_HOST
 
 php-fpm81 -F
