8dbad9eca6a381109b7124aaeb903245d13edefe
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-05-17 14:03:06 +0400

    feat: checking for wp core installation

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+## Wordpress Setup
+echo "[info] Setting up Wordpress"
+
 echo "[info] installing wp-cli"
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/bin/wp
 
-echo "[info] downloading wordpress files"
-wp core download --allow-root
+if ! wp core is-installed; then
+    echo "[info] wp is not installed, downloading wordpress files"
+    wp core download --allow-root
+else
+    echo "[info] wp is already installed"
+fi
+
 
 php-fpm81 -F
